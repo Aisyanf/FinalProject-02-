c3ef8980338c4a6282a8cd77b79c856a95d2e907
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-09-25 10:35:07 +0700

    amel

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        
+
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi Ais");
-         System.out.println("Ais menambahkan baris 1");
+        System.out.println("Hi, ini baris kode kedua yang dimodifikasi Amel");
+        System.out.println("Ais menambahkan baris 1");
     }
 }
