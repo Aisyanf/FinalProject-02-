abd786f120a5dfed530fb6db178168bbfa87de41
AmaliaAfidatulAini <140462288+AmaliaAfidatulAini@users.noreply.github.com>
2025-09-25 10:11:38 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi Ais");
         System.out.println("Ais menambahkan baris baru");
-         System.out.println("amel menambahkan baris baru");
+         System.out.println("ameel menambahkan baris baru");
     }
 }
