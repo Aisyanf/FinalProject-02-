b716d7a092af4c1978d1970d4117ee141c3efdbb
Aisyanf <an.fardani@gmail.com>
2025-09-25 10:30:36 +0700

    Pertama

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi Ais");
-        System.out.println("Ais menambahkan baris baru");
+        
+        System.out.println("Hi, ini baris kode pertama");
     }
 }
