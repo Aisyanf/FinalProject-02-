75bb6f1cb5cdcd63c8c265455d329e21ea096a7a
Aisyanf <an.fardani@gmail.com>
2025-09-25 10:32:29 +0700

    Ais

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         
-        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi Ais");
+         System.out.println("Ais menambahkan baris 1");
     }
 }
